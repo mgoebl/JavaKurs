41f94d9d2a805a1467ae87408984dcd3fc6b5fcd
Michael Göbl <michael.goebl@gruenholzwerkstatt.de>
2019-10-18 21:27:01 +0200

    jetzt mit nested loop um 5 Würfe abzubilden und 20 Schritte zu gehen. Wenn genau 20 Schritte erzielt werden gewinnt man. Ansonsten verliert man.

--- src/chapter4/RollTheDieGame.java
+++ src/chapter4/RollTheDieGame.java
@@ -7,13 +7,7 @@ public class RollTheDieGame {
     public static void main(String[] args) {
         int lastSpace = 20;
         int currentSpace = 0;
-        int maxRolls = 15000000;
-        int sechser = 0;
-        int fuenfer = 0;
-        int vierer = 0;
-        int dreier = 0;
-        int zweier = 0;
-        int einer = 0;
+        int maxRolls = 6;
         Random random = new Random();
 
         System.out.println("Welcome to Roll the Die! Let's begin...");
@@ -23,35 +17,28 @@ public class RollTheDieGame {
             int die = random.nextInt(6) + 1;
             currentSpace = currentSpace + die;
 
-            if (die == 6) {
-                sechser = sechser + 1;
+            System.out.print(String.format("Roll #%d: You've rolled a %d. ", i, die));
+
+            if(currentSpace == lastSpace){
+                System.out.print("You're on space " + currentSpace + ". Congrats, you win!");
+                break;
+            }
+            else if(currentSpace > lastSpace){
+                System.out.print("Unfortunately, that takes you past " + lastSpace + " spaces. You lose!");
+                break;
+            }
+            else if(i == maxRolls){
+                System.out.println("You're on space " + currentSpace + ".");
+                System.out.println("Unfortunately, you didn't make it to all " +
+                        lastSpace + " spaces. You lose!");
+            }
+            else{
+                int spacesToGo = lastSpace - currentSpace;
+                System.out.print("You are now on space " + currentSpace +
+                        " and have " + spacesToGo + " more to go.");
             }
 
-            else if (die == 5){
-                fuenfer = fuenfer + 1;
-            }
-
-            else if (die == 4){
-                vierer = vierer + 1;
-            }
-
-            else if (die == 3){
-                dreier = dreier + 1;
-            }
-
-            else if (die == 2){
-                zweier = zweier + 1;
-            }
-
-            else if (die == 1){
-                einer = einer + 1;
-            }
+            System.out.println();
         }
-        System.out.print("Sechser gewürfelt:" + sechser + System.lineSeparator());
-        System.out.print("Fünfer gewürfelt:" + fuenfer + System.lineSeparator());
-        System.out.print("Vierer gewürfelt:" + vierer + System.lineSeparator());
-        System.out.print("Dreier gewürfelt:" + dreier + System.lineSeparator());
-        System.out.print("Zweier gewürfelt:" + zweier + System.lineSeparator());
-        System.out.print("Einer gewürfelt:" + einer);
     }
 }
