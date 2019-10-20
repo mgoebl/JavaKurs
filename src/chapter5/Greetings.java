144498c076d73bf3d4369af4d8af2ca0011f2375
Michael Göbl <michael.goebl@gruenholzwerkstatt.de>
2019-10-20 10:55:36 +0200

    Methode erweitert um Nachname Methodendeklaration auf "private" geändert

---
+++ src/chapter5/Greetings.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+import java.util.Scanner;
+
+/*
+ * OUR FIRST METHOD
+ * Write a method that asks a user for their name, then greets them by name.
+ */
+public class Greetings {
+
+    public static void main(String[] args){
+
+        System.out.println("Gib Deinen Namen ein:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        String nachName = scanner.next();
+        scanner.close();
+        greetUser(name, nachName);
+    }
+
+    private static void greetUser(String name, String nachName){
+
+        System.out.println("Hallo, " + name + ", " + nachName);
+    }
+}
