8fe34cb9e038bb604c2bc2c42e994f0a9475adf8
Michael Göbl <michael.goebl@gruenholzwerkstatt.de>
2019-10-21 17:02:46 +0200

    Gültigkeit von Variablen. Definition als static in der Java-class -> Variablen sind in den Methoden verfügbar.

---
+++ src/chapter5/SofortKreditCheck.java
@@ -0,0 +1,46 @@
+package chapter5;
+
+/* schreibe ein Programm, welches jedem einen Kredit gewährt, der mehr als 25000€ verdient und einen
+Kreditscore von mindestens 700 hat. Alle anderen bekommen keinen Kredit.
+ */
+
+import java.util.Scanner;
+
+public class SofortKreditCheck {
+
+    private static int benoetigtesEinkommen = 25000;
+    private static int benoetigterKreditScore = 700;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        double einkommen = holeEinkommen();
+        int kreditScore = holeKreditScore();
+        scanner.close();
+        // Prüfe ob der Benutzer für einen Kredit qualifiziert ist
+        boolean istBenutzerQualifiziert = istBenutzerQualifiziert(kreditScore, einkommen);
+        informiereBenutzer(istBenutzerQualifiziert);
+    }
+private static double holeEinkommen() {
+        System.out.println("Gebe dein Einkommen ein:");
+        return scanner.nextDouble();
+    }
+
+private static int holeKreditScore() {
+        System.out.println("Gebe deinen Kreditscore ein:");
+        return scanner.nextInt();
+    }
+
+private static boolean istBenutzerQualifiziert(int kreditScore, double einkommen) {
+    return kreditScore >= benoetigterKreditScore && einkommen >= benoetigtesEinkommen;
+    }
+
+private static void informiereBenutzer(boolean istQualifiziert) {
+        if (istQualifiziert) {
+            System.out.println("Sie bekommen einen Kredit!");
+        }
+        else{
+                System.out.println("Ihnen wird kein Kredit gewährt.");
+        }
+    }
+}
