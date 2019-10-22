290d7bea1630f05670362e28e7e99240e15c1fbb
Michael Göbl <michael.goebl@gruenholzwerkstatt.de>
2019-10-22 18:20:29 +0200

    zwei Objekte werden instanziiert und Methode "berechneFlaeche" aufgerufen und mit den Ergebnissen gerechnet.

---
+++ src/chapter6/WohnFlaechenRechner.java
@@ -0,0 +1,22 @@
+package chapter6;
+
+public class WohnFlaechenRechner {
+
+    public static void main(String[] args){
+        /*
+         Rechteck 1
+        */
+        Rectangle raum1 = new Rectangle();
+        raum1.setzeLaenge(50);
+        raum1.setzeBreite(20);
+        double raum1Flaeche = raum1.berechneFlaeche();
+        /*
+        Rechteck 2
+        */
+        Rectangle raum2 = new Rectangle(150, 120);
+        double raum2Flaeche = raum2.berechneFlaeche();
+        double  raum1undraum2 = raum1Flaeche + raum2Flaeche;
+
+        System.out.println("Beide Räume haben eine Fläche von:" + raum1undraum2);
+    }
+}
